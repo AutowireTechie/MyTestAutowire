372e2cc7b1ed16791b4c9a201d3aeeac733170d4
shibashankar <shibashankar@DESKTOP-P7IN00B>
2020-02-06 15:31:11 +0530

    First Commit data

---
+++ SonarTest/src/main/java/com/sonartest/SonarTest/App.java
@@ -0,0 +1,32 @@
+package com.sonartest.SonarTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omg.CORBA.INTERNAL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	
+    public App() {
+		System.out.println("Hi");
+	}
+    
+    public void test() {
+    	List<Integer> val = new ArrayList<Integer>();
+    	val.add(1);
+    	val.add(2);
+    }
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        App ap = new App();
+        ap.test();
+        
+    }
+}
